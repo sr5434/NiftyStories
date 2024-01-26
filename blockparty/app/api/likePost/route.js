5e1b09c1ee99c0fead2240534bda161d4202cdbb
Samir <samirrangwalla1@outlook.com>
2024-01-25 19:25:57 -0500

    Add parts of the like feature

--- blockparty/app/api/likePost/route.js
+++ blockparty/app/api/likePost/route.js
@@ -5,16 +5,16 @@ const sql = require('mssql');
 
 export async function POST(req){
   const {userId} = getAuth(req);
- 
+
   if(!userId){
     return new Response("Unauthorized", { status: 401 });
   }
   let reqJSON = await req?.json();
   //Connect to database
   await sql.connect(process.env.CONN_STR);
-  const PostID = uuidv4();
-  const result = await sql.query`INSERT INTO [Posts].[Post] (PostID, [User], Content, Timestamp) VALUES (${PostID}, ${userId}, ${reqJSON.post}, GETDATE()); INSERT INTO [Likes].[Post] (PostID, Likes) VALUES (${PostID}, 0);`;
-
+  const result = await sql.query`IF EXISTS (SELECT 1 FROM [Likes].[Post] WHERE PostID = ${reqJson.postID}) UPDATE [Likes].[Post] SET Likes+=1 WHERE PostID = ${reqJson.postID} ELSE INSERT INTO [Likes].[Post] (PostID, Likes) VALUES (${reqJson.postID}, 0)`;
+  //TODO: Add to list of liked posts
+  const result2 = await sql.query``
   // Sample curl request to test this: curl -X POST -H "Content-Type: application/json" -d '{"post":"..."}' http://localhost:3000/api/newPost
-  return NextResponse.json({ result: result });
+  return NextResponse.json({ });
 }
